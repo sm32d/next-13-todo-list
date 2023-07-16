cdc03c6a622db8c287ba834cd9cb1e273d96a367
Sulabh Mehta (Dominik) <64636803+sm32d@users.noreply.github.com>
2023-07-16 17:44:43 +0800

    added delete functionality

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,17 +1,17 @@
-import { prisma } from "@/db"
-import { redirect } from "next/navigation"
-import Link from "next/link"
+import { prisma } from "@/db";
+import { redirect } from "next/navigation";
+import Link from "next/link";
 
 async function createTodo(data: FormData) {
-  "use server"
+  "use server";
 
-  const title = data.get("title")?.valueOf()
+  const title = data.get("title")?.valueOf();
   if (typeof title !== "string" || title.length === 0) {
-    throw new Error("Invalid Title")
+    throw new Error("Invalid Title");
   }
 
-  await prisma.todo.create({ data: { title, complete: false } })
-  redirect("/")
+  await prisma.todo.create({ data: { title, complete: false } });
+  redirect("/");
 }
 
 export default function Page() {
@@ -20,13 +20,13 @@ export default function Page() {
       <header className="flex justify-between items-center mb-4">
         <h1 className="text-2xl">New</h1>
       </header>
-      <form action={createTodo} className="flex gap-2 flex-col">
+      <form action={createTodo} className="flex gap-3 flex-col">
         <input
           type="text"
           name="title"
           className="border border-slate-300 bg-transparent rounded px-2 py-1 outline-none focus-within:border-slate-100"
         />
-        <div className="flex gap-1 justify-end">
+        <div className="flex gap-3 justify-end">
           <Link
             href=".."
             className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
@@ -42,5 +42,5 @@ export default function Page() {
         </div>
       </form>
     </>
-  )
+  );
 }
--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,28 +1,50 @@
-"use client"
+"use client";
+
+import { useRouter } from "next/navigation";
 
 type TodoItemProps = {
-  id: string
-  title: string
-  complete: boolean
-  toggleTodo: (id: string, complete: boolean) => void
-}
+  id: string;
+  title: string;
+  complete: boolean;
+  toggleTodo: (id: string, complete: boolean) => void;
+  deleteTodo: (id: string) => void;
+};
 
-export function TodoItem({ id, title, complete, toggleTodo }: TodoItemProps) {
+export function TodoItem({
+  id,
+  title,
+  complete,
+  toggleTodo,
+  deleteTodo,
+}: TodoItemProps) {
+  const router = useRouter();
+
   return (
-    <li className="flex gap-1 items-center">
+    <li className="flex gap-1 items-center justify-between py-2">
+      <div className="flex gap-2 items-center">
+        <input
+          id={id}
+          type="checkbox"
+          className="cursor-pointer peer"
+          defaultChecked={complete}
+          onChange={(e) => toggleTodo(id, e.target.checked)}
+        />
+        <label
+          htmlFor={id}
+          className="cursor-pointer peer-checked:line-through peer-checked:text-slate-500"
+        >
+          {title}
+        </label>
+      </div>
       <input
-        id={id}
-        type="checkbox"
-        className="cursor-pointer peer"
-        defaultChecked={complete}
-        onChange={e => toggleTodo(id, e.target.checked)}
+        className="border border-slate-300 text-slate-300 px-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
+        type="button"
+        value="🗑️"
+        onClick={(e) => {
+          deleteTodo(id);
+          router.refresh();
+        }}
       />
-      <label
-        htmlFor={id}
-        className="cursor-pointer peer-checked:line-through peer-checked:text-slate-500"
-      >
-        {title}
-      </label>
     </li>
-  )
+  );
 }
